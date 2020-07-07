906172a4985ec53fe0864940b66c4f4116518c65
Manav Parmar <parmar.manav2000@gmail.com>
2020-07-07 14:18:39 +0530

    export to csv

--- orders/views.py
+++ orders/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
+from django.contrib.admin.views.decorators import staff_member_required
 from .models import *
 from .forms import *
 from cart.cart import Cart
@@ -28,4 +29,12 @@ def order_create(request):
     return render(request, 'orders/order/create.html', {
         'cart': cart,
         'form': form
+    })
+
+
+@staff_member_required
+def admin_order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    return render(request, 'admin/orders/order/detail.html', {
+        'order': order
     })
